f8481932d1703767d37a2bb5096371ab59e68d06
AmyPhung <amyngph@gmail.com>
2018-11-03 12:59:45 -0400

    Added function to button

--- html5up-helios/__init__.py
+++ html5up-helios/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, request, render_template
+from EmailTest import sendEmailTo
 import json
 # from flask_wtf import Form
 # from wtforms import TextField
@@ -9,16 +10,22 @@ app = Flask(__name__)
 # adjust is modifier
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST', 'GET']) #
 def index(emailaddress=None):
-    email = request.args['emailaddress']
-    print("it worked!")
+    if request.method == 'POST':
+        email = request.form["emailaddress"]
+        print(email)
+        sendEmailTo(email)
+        print("it worked!")
+    #pass
     return render_template('index.html')
 
 @app.route('/done/')
 def about():
     return render_template('done.html')
 
+if __name__ == "__main__":
+    app.run('0.0.0.0', debug=True)
 
 """
 @app.route('/display/', methods = ['POST','GET'])
